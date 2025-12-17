2d06b33d126d1dea1efbec195555c95591062635
Andrea Duarte <ajduarteg@uamv.edu.ni>
2025-12-16 21:17:17 -0600

    add interface crud

---
+++ sesion5/src/main/java/repositorio/CrudRepositorio.java
@@ -0,0 +1,14 @@
+package repositorio;
+
+import java.util.List;
+
+public interface CrudRepositorio<Producto> {
+
+    List<Producto> listar();
+
+    Producto guardar(Producto producto);
+
+    Producto buscarPorId(Integer id);
+
+    void eliminar(Integer id);
+}
