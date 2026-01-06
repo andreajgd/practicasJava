082d19d9488c1c686dbcf279eb0b38a9529c8f03
Andrea Duarte <ajduarteg@uamv.edu.ni>
2026-01-05 20:33:28 -0600

    add Usuario

---
+++ demoFilterJPA/src/main/java/model/Usuario.java
@@ -0,0 +1,27 @@
+package model;
+
+import config.IUsuario;
+import jakarta.persistence.Entity;
+
+@Entity
+public class Usuario extends BaseEntity implements IUsuario {
+    private String nombre;
+    private String apellido;
+
+    public String getNombre() {
+
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+}
