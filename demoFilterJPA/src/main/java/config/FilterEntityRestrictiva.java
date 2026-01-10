8684d2ffcc01c1347a33bfe98af9f5eba90abcb8
Andrea Duarte <ajduarteg@uamv.edu.ni>
2026-01-09 19:53:11 -0600

    update FilterEntityRestrictiva

--- demoFilterJPA/src/main/java/config/FilterEntityRestrictiva.java
+++ demoFilterJPA/src/main/java/config/FilterEntityRestrictiva.java
@@ -5,34 +5,42 @@ import jakarta.persistence.Table;
 import jakarta.persistence.metamodel.EntityType;
 import jakarta.persistence.metamodel.Metamodel;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.HashMap;
+import java.util.Map;
 
 public class FilterEntityRestrictiva {
 
-    private static final Set<String> FILTERED_TABLES = new HashSet<String>();
+    private static final Map<String,Campo> FILTERED_TABLES = new HashMap<>();
 
     public static void registerEntity(EntityManagerFactory emf) {
         Metamodel metamodel = emf.getMetamodel();
         for (EntityType<?> entity : metamodel.getEntities()) {
             Class<?> javaType = entity.getJavaType();
-            if (IRestrictiva.class.isAssignableFrom(javaType)) {
+            if (javaType.isAnnotationPresent(RestrictivaUsuario.class))  {
+                Table table = javaType.getAnnotation(RestrictivaUsuario.class).tipo();
+                String value = javaType.getAnnotation(RestrictivaUsuario.class).valor();
                 Table table = javaType.getAnnotation(Table.class);
                 if (table != null && !table.name().isBlank()) {
-                    FILTERED_TABLES.add(table.name().toLowerCase());
+                    FILTERED_TABLES.put(table.name().toLowerCase()
+                            , new Campo(campo,restrictionType,value) );
                 }
                 else {
-                    FILTERED_TABLES.add(javaType.getSimpleName());
+                    FILTERED_TABLES.put(javaType.getSimpleName.toLowerCase(),
+                            new Campo(campo,restrictionType,value));
                 }
             }
         }
     }
 
     public static boolean siFilter(String tableName) {
-        return FILTERED_TABLES.contains(tableName.toLowerCase());
+        return FILTERED_TABLES.containsKey(tableName.toLowerCase());
     }
 
     public static int getSizeFilterTable() {
         return FILTERED_TABLES.size();
+    }
+
+    public static Map<String,Campo> getFilteredTables() {
+        return FILTERED_TABLES;
     }
 }
