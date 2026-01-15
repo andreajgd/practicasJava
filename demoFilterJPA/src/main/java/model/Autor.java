84544118f44b8109877cecd1c76c817e79e9c273
Andrea Duarte <ajduarteg@uamv.edu.ni>
2026-01-15 15:33:50 -0600

    add Restrictivas Autor

--- demoFilterJPA/src/main/java/model/Autor.java
+++ demoFilterJPA/src/main/java/model/Autor.java
@@ -1,8 +1,24 @@
 package model;
 
+import config.RestrictionType;
+import config.Restrictiva;
+import config.RestrictivaUsuario;
 import jakarta.persistence.Entity;
+import jakarta.persistence.NamedQuery;
+import jakarta.persistence.Table;
 
 @Entity
+@NamedQuery(
+        name="Autor.all"
+        ,query="select e from Autor e"
+)
+@Table(name="Autor")
+@Restrictiva(campo = "id")
+@RestrictivaUsuario(
+        campo = "id",
+        tipo = RestrictionType.CURRENT_USER
+)
+
 public class Autor extends BaseEntity {
     private String nombre;
     private String apellido;
@@ -22,4 +38,5 @@ public class Autor extends BaseEntity {
     public void setApellido(String apellido) {
         this.apellido = apellido;
     }
+
 }
